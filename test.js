9fa6dc23fdfc0b3a9e7f12b90cee12125b168b4d
mekhroj <161313162+mekhrojY@users.noreply.github.com>
2024-03-08 19:31:44 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+//arrayga misollar
+var cars = ["Audi", "Benz", "Bmw", "Kia", "Hyundai"]
+var carmodels = ["A6","CLS", "X5", "Carnival", "Palesade" ]
+//1) o'zgaruvchidagi arraylar uzunligi va ularga ihtiyoriy push bn misol qo'shish. 
+
+//console.log(cars.push("Chevrolet"));
+//console.log(cars)
+
+//2)O'zgaruvchidagi arraylarga unshift bn ixtiyoriy qo'shimcha qilish.
+
+//console.log(carmodels.unshift("Damas"));
+//console.log(carmodels)
+
+//3)pop va shiftga misol
+//console.log(cars.pop())
+//console.log(cars.shift())
+//console.log(cars)
+
+ 
+
+
+// 4)Ikta o'zgaruvchidan concatga,sort,splicega misol
+//console.log(cars.concat(carmodels))
+
+//console.log(cars.concat(carmodels).sort().splice(1))
+
+
